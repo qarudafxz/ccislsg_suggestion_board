176f1d49273861ea9b894c812de99bf67ad51d62
qaruda <70809588+francistinao@users.noreply.github.com>
2023-07-08 17:27:36 +0800

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics";
 
 //pages
 import Home from "./pages/Home";
@@ -13,6 +14,7 @@ function App() {
 					element={<Home />}
 				/>
 			</Routes>
+			<Analytics />
 		</Router>
 	);
 }
